286d98e2b8cd6638b5a8c6f3f891bc2d0bf957aa
IanMcCauley <imccauley18@georgefox.edu>
2021-12-01 18:14:25 -0800

    final project

---
+++ FinalProject/script.js
@@ -0,0 +1,160 @@
+var api = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
+let ticker;
+var apiKey = "&apikey=ZW0CYUY667IBGA7N";
+var stocks = [];
+var dps = [];
+var myChart;
+
+let today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+var yyyy = today.getFullYear();
+
+today = yyyy + '-' + mm + '-' + dd;
+
+/*This initiates a scrolling ticker from CSS Script library 
+https://www.cssscript.com/smooth-marquee-like-scroller-pure-javascript-marquee3000/
+Author: http://github.com/ezekielaquino/marquee3000*/
+
+window.addEventListener("load", marqueeData);
+function marqueeData(){
+fetch(api + "AAPL" + apiKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+    fetch(api + "IBM" + apiKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+    fetch(api + "TSLA" + apiKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+    fetch(api + "HD" + apiKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+    fetch(api + "PG" + apiKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+}
+
+function appendData(data) {
+    stocks.push(data["Time Series (Daily)"][today]["2. high"]);
+    console.log(stocks);
+    if(stocks.length == 5){
+        document.getElementById("marquee").innerHTML = " || AAPL: $" + stocks[0].toString() + " || IBM: $" + stocks[1].toString() + " || TSLA: $" + stocks[2].toString() + " || HD: $" + stocks[3].toString() + " || PG: $" + stocks[4].toString();
+        Marquee3k.init();
+    }
+}
+
+
+function getData(){
+    ticker = document.getElementById("ticker").value;
+    var url = api + ticker + apiKey;
+
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        gatherData(data);
+    });
+}
+
+function gatherData(data){
+    var points = [];
+    var labels = [];
+    var date = data["Time Series (Daily)"]
+      let a = 40;
+      let b = 30;
+      for(var d in date){
+        var r = d.split("-");
+        if(a-- > 0){
+          var value = date[d];
+          dps.unshift({x: new Date(parseInt(r[0]), parseInt(r[1])-1, parseInt(r[2])), y: parseFloat(value["1. open"])});
+          if(b-- > 0){
+            let c = [d, value["2. high"]];
+            points.push(value["2. high"]);
+            labels.push(d);
+          }
+        }else{
+          break;
+          }
+        }
+console.log(points);
+console.log(labels.reverse());
+createGraph(labels,points);
+}
+
+    
+    
+
+function createGraph(labels,points){
+    const DATA = {
+        labels: labels,
+        datasets: [{
+            label: "Daily high for ($)" + ticker,
+            backgroundColor  : 'rgb(100, 99, 132,0.2)',
+            borderColor      : 'rgb(63, 242, 153)',
+            color            : 'rgb(63, 242, 153)',
+            family           : 'Helvetica',
+            pointHitRadius   : 10,
+            data             : points.reverse(),
+            fill             : true,
+            tension          : 0.1
+        }]
+      };
+
+    const config = {
+        type: 'line',
+        data: DATA,
+        options: {
+            
+        }
+      };
+
+      if (myChart != null) {
+      myChart.destroy();}
+
+    myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
+}
+
